52225f41deaa0af82c0d28f4d7f510cd42460a1d
ParkChan <hydrarkk@naver.com>
2024-03-07 01:50:17 +0900

    [추가] 간단한 웹뷰로딩 추가

--- app/src/main/java/com/chan/subwayrrealtime/MainActivity.kt
+++ app/src/main/java/com/chan/subwayrrealtime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.chan.subwayrrealtime
 
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.chan.subwayrrealtime.ui.theme.SubwayrRealTimeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,21 +24,43 @@ class MainActivity : ComponentActivity() {
             SubwayrRealTimeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WebViewScreen(loadUrl)
                 }
             }
         }
     }
+    companion object {
+        private const val loadUrl = "https://smss.seoulmetro.co.kr/traininfo/traininfoUserView.do"
+    }
+}
+@Composable
+fun WebViewScreen(url: String) {
+    AndroidView(factory = { context ->
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            webViewClient = object : WebViewClient(){
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    return super.shouldOverrideUrlLoading(view, request)
+                }
+            }
+            settings.loadWithOverviewMode = true
+            settings.setSupportZoom(true)       // 화면 줌 허용여부
+            setInitialScale(125)
+        }
+    }, update = { webView ->
+        webView.loadUrl(url)
+    })
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
